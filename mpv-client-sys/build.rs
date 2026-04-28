2504219545c92189aea0989d98664cd4ba57a2c5
batosgi <novak34291@gmail.com>
2026-04-28 22:25:34 +0300

    build(sys): use pregenerated bindings, remove bindgen
    Simplifies build by shipping pregenerated FFI bindings instead of
    generating at compile time. Removes bindgen dependency and all
    transitive deps.

--- mpv-client-sys/build.rs
+++ mpv-client-sys/build.rs
@@ -2,13 +2,14 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .generate()
-        .expect("Unable to generate bindings");
-
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    let crate_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    std::fs::copy(
+        crate_path.join("pregenerated_bindings.rs"),
+        out_path.join("bindings.rs"),
+    )
+    .expect("Couldn't find pregenerated bindings!");
+
+    println!("cargo:rustc-link-search=native={}", crate_path.display());
+    println!("cargo:rustc-link-lib=mpv");
 }
